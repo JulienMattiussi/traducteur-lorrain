fb6e41eae8fc65b479d590029409b55f2212eb9f
JulienM <julienm@marmelab.com>
2022-04-19 15:03:14 +0200

    set lorraine

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img
+    alt="Lorraine logo"
+    class="logo"
+    src="./assets/Blason_Lorraine.svg.png"
+  />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
@@ -21,6 +25,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
+}
+
+.logo {
+  width: 50px;
 }
 </style>
